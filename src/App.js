3f3858695e22649c633cdbf56d28e3a08a3a2c9b
unknown <sebas907.l@gmail.com>
2022-04-27 23:06:08 -0500

    Se ha fixeado el error en la re-renderizacion, ahora se esta trabajando en la eliminacion de tareas

--- src/App.js
+++ src/App.js
@@ -1,55 +1,56 @@
-import React from 'react';
-import { ToDoCounter } from './components/ToDoCounter';
-import { ToDoSearch } from './components/ToDoSearch';
-import { ToDoList } from './components/ToDoList';
-import { ToDoItem } from './components/ToDoItem';
+import React from "react";
+import { ToDoCounter } from "./components/ToDoCounter";
+import { ToDoSearch } from "./components/ToDoSearch";
+import { ToDoList } from "./components/ToDoList";
+import { ToDoItem } from "./components/ToDoItem";
 
 const defaultToDo = [
-    {text: 'Escucho a un bebe llorar en mi casa', completed: false},
-    {text: 'Me levanto a revisar', completed: false},
-    {text: '*Vivo solo*', completed: true}
-]
+  { text: "Escucho a un bebe llorar en mi casa", completed: false },
+  { text: "Me levanto a revisar", completed: false },
+  { text: "*Vivo solo*", completed: true },
+];
 
 function App() {
-  const [toDo, setToDo] = React.useState(defaultToDo)
-  const [textInput, setInputValue] = React.useState('');
+  const [toDo, setToDo] = React.useState(defaultToDo);
+  const [textInput, setInputValue] = React.useState("");
 
-  const completeTasks = toDo.filter( toDo => !!toDo.completed ).length;
+  const completeTasks = toDo.filter((toDo) => !!toDo.completed).length; //Objeto en Default toDo
   const totalTask = toDo.length;
 
   let searchedTodos = [];
 
-  if(!textInput.length >= 1){
+  if (!textInput.length >= 1) {
     searchedTodos = toDo;
   } else {
-    searchedTodos = toDo.filter( toDo => {
+    searchedTodos = toDo.filter((toDo) => {
       const toDoText = toDo.text.toLowerCase();
       const toDoInput = textInput.toLowerCase();
-      return toDoText.includes(toDoInput)
-    })
+      return toDoText.includes(toDoInput);
+    });
   }
+
+  const checkCompleteTasks = (text) => { 
+    const toDoIndex = toDo.findIndex((toDo) => toDo.text === text); 
+    const newArrToDo = [...toDo];
+    newArrToDo[toDoIndex].completed = true;
+    setToDo(newArrToDo);
+  };
 
   return (
     <React.Fragment>
-      <ToDoCounter /> 
-      <ToDoList
-      totalTask = {totalTask}
-      completed = {completeTasks}
-      >
-      <ToDoSearch 
-      textInput={textInput}
-      setInputValue={setInputValue}
-      />
-          {searchedTodos.map ( toDo => (
-              <ToDoItem 
-              text={toDo.text}
-              key={toDo.text}
-              completed={toDo.completed}
-              />)
-          )}
+      <ToDoCounter />
+      <ToDoList totalTask={totalTask} completed={completeTasks}>
+        <ToDoSearch textInput={textInput} setInputValue={setInputValue} />
+        {searchedTodos.map((toDo) => (
+          <ToDoItem
+            text={toDo.text}
+            key={toDo.text}
+            completed={toDo.completed} 
+            onComplete={() => checkCompleteTasks(toDo.text)}
+          />
+        ))}
       </ToDoList>
     </React.Fragment>
-
   );
 }
 
--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -2,15 +2,15 @@ import React from "react";
 import "../style/ToDoItem.css";
 
 function ToDoItem(props) {
-  const onComplete = () => {
-    console.log('right now, youre the best person in the entire world!')
-  }
+  // const onComplete = () => {
+  //   console.log('right now, youre the best person in the entire world!')
+  // }
 
   return (
     <li className="toDoItem">
       <span
         className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}
-        onClick={onComplete}
+        onClick={props.onComplete} 
       >
         o
       </span>
