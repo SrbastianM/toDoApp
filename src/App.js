ae3928e1a9ec8ea667fee3cca0bb62442dead681
unknown <sebas907.l@gmail.com>
2022-04-28 15:36:06 -0500

    Se re formulo la componetizacion para respetar los parametros aprendidos en la clase, ocasiono que oslo funcione a la mitad el contador de tareas, pero, se esta trabajando en una solucion

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from "react";
-import { ToDoCounter } from "./components/ToDoCounter";
-import { ToDoSearch } from "./components/ToDoSearch";
-import { ToDoList } from "./components/ToDoList";
-import { ToDoItem } from "./components/ToDoItem";
+import { AppUI } from "./components/AppUI";
 
-const defaultToDo = [
-  { text: "Escucho a un bebe llorar en mi casa", completed: false },
-  { text: "Me levanto a revisar", completed: false },
-  { text: "*Vivo solo*", completed: true },
-];
+// const defaultToDo = [
+//   { text: "Escucho a un bebe llorar en mi casa", completed: false },
+//   { text: "Me levanto a revisar", completed: false },
+//   { text: "*Vivo solo*", completed: true },
+// ];
 
 function App() {
-  const [toDo, setToDo] = React.useState(defaultToDo);
+  const localStorageToDo = localStorage.getItem('TODO_V1');
+  let parsedToDo;
+
+  if(!localStorageToDo) {
+    localStorageToDo.setItem('TODO_V1', JSON.stringify([]));
+    parsedToDo = [];
+  } else {
+    parsedToDo = JSON.parse(localStorageToDo);
+  }
+
+  const [toDo, setToDo] = React.useState(parsedToDo);
   const [textInput, setInputValue] = React.useState("");
 
   const completeTasks = toDo.filter((toDo) => !!toDo.completed).length; //Objeto en Default toDo
@@ -44,24 +51,15 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-      <ToDoCounter 
-      total={totalTask}
-      completed={completeTasks}
-      />
-      <ToDoList totalTask={totalTask} completed={completeTasks}>
-        <ToDoSearch textInput={textInput} setInputValue={setInputValue} />
-        {searchedTodos.map((toDo) => (
-          <ToDoItem
-            text={toDo.text}
-            key={toDo.text}
-            completed={toDo.completed} 
-            onComplete={() => checkCompleteTasks(toDo.text)}
-            onDelete={() => deleteTasks(toDo.text)}
-          />
-        ))}
-      </ToDoList>
-    </React.Fragment>
+    <AppUI 
+    totalTask={totalTask}
+    completedTasks={completeTasks}
+    textInput={textInput} 
+    setInputValue={setInputValue}
+    searchedTodos={searchedTodos}
+    checkCompleteTasks={checkCompleteTasks}
+    deleteTasks={deleteTasks}
+    />
   );
 }
 
---
+++ src/components/AppUI.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { ToDoCounter } from "../components/ToDoCounter";
+import { ToDoSearch } from "../components/ToDoSearch";
+import { ToDoList } from "../components/ToDoList";
+import { ToDoItem } from "../components/ToDoItem";
+
+function AppUI({
+    totalTask,
+    completeTasks,
+    textInput,
+    setInputValue,
+    searchedTodos,
+    checkCompleteTasks,
+    deleteTasks
+}) {
+  return (
+    <React.Fragment>
+      <ToDoCounter total={totalTask} completed={completeTasks} />
+      <ToDoList totalTask={totalTask} completed={completeTasks}>
+        <ToDoSearch textInput={textInput} setInputValue={setInputValue} />
+        {searchedTodos.map((toDo) => (
+          <ToDoItem
+            text={toDo.text}
+            key={toDo.text}
+            completed={toDo.completed}
+            onComplete={() => checkCompleteTasks(toDo.text)}
+            onDelete={() => deleteTasks(toDo.text)}
+          />
+        ))}
+      </ToDoList>
+    </React.Fragment>
+  );
+}
+
+export { AppUI };
