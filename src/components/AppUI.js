624285b7e19311c5818f53b99c39adf09b0613a6
unknown <sebas907.l@gmail.com>
2022-05-10 08:16:25 -0500

    Cambios realizados para el uso del context, (aunque eso breakeo todo)

--- src/components/AppUI.js
+++ src/components/AppUI.js
@@ -6,28 +6,35 @@ import { ToDoList } from "../components/ToDoList";
 import { ToDoItem } from "../components/ToDoItem";
 
 function AppUI() {
+  const {
+    loading,
+    error,
+    totalTask,
+    completeTasks,
+    textInput,
+    setInputValue,
+    searchedTodos,
+    checkCompleteTasks,
+    deleteTasks,
+  } = React.useContext(ToDoContext);
   return (
     <React.Fragment>
       <ToDoCounter />
-      <ToDoContext.Consumer>
-        {({ error, loading, completeTasks, deleteTasks }) => (
-          <ToDoList totalTask={totalTask} completed={completeTasks}>
-            <ToDoSearch />
-            {error && <p>Loading the aplication!</p>}
-            {loading && <p>Loading the aplication!</p>}
-            {!loading && !searchedTodos.lenght && <p>Create your first ToDo</p>}
-            {searchedTodos.map((toDo) => (
-              <ToDoItem
-                text={toDo.text}
-                key={toDo.text}
-                completed={toDo.completed}
-                onComplete={() => checkCompleteTasks(toDo.text)}
-                onDelete={() => deleteTasks(toDo.text)}
-              />
-            ))}
-          </ToDoList>
-        )}
-      </ToDoContext.Consumer>
+      <ToDoList totalTask={totalTask} completed={completeTasks}>
+        <ToDoSearch />
+        {error && <p>Loading the aplication!</p>}
+        {loading && <p>Loading the aplication!</p>}
+        {!loading && !searchedTodos.lenght && <p>Create your first ToDo</p>}
+        {searchedTodos.map((toDo) => (
+          <ToDoItem
+            text={toDo.text}
+            key={toDo.text}
+            completed={toDo.completed}
+            onComplete={() => checkCompleteTasks(toDo.text)}
+            onDelete={() => deleteTasks(toDo.text)}
+          />
+        ))}
+      </ToDoList>
     </React.Fragment>
   );
 }
